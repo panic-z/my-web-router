8485d0057207e153370733bca6c796daf0837101
Daniel <daniel@lightleap.cn>
2026-04-10 10:59:41 +0800

    test: bound portal support link assertion

--- tests/feedback/portal-index-html.test.js
+++ tests/feedback/portal-index-html.test.js
@@ -6,7 +6,7 @@ const { join } = require("node:path");
 const html = readFileSync(join(__dirname, "../../index.html"), "utf8");
 
 test("portal homepage keeps feedback out of the primary card nav", () => {
-  assert.match(html, /<p class="support-copy">[\s\S]*href="\/feedback\?product=portal"/);
+  assert.match(html, /<p class="support-copy">[\s\S]*?href="\/feedback\?product=portal"[\s\S]*?<\/p>/);
   assert.doesNotMatch(html, /<nav>[\s\S]*href="\/feedback\?product=portal"/);
   assert.match(html, /data-i18n="supportPrompt"/);
 });
